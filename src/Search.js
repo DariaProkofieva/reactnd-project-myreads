5379f5305f5a9839a4568bd8aefd471100859830
Daria Prokofieva <dashaprokofieva95@gmail.com>
2019-08-30 17:24:08 +0200

    feat: add search input

--- src/Search.js
+++ src/Search.js
@@ -2,13 +2,24 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 class Search extends React.Component {
+  state = {
+    query: '',
+  }
+  updateQuery = (query) => {
+    this.setState(() => ({
+      query: query
+    }))
+  }
   render() {
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <Link to='/' className="close-search">Close</Link>
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author"/>
+            <input type="text"
+                   placeholder="Search by title or author"
+                   value={this.state.query}
+                   onChange={(event) => this.updateQuery(event.target.value)}/>
           </div>
         </div>
         <div className="search-books-results">
