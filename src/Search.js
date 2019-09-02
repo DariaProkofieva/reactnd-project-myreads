2e4f0691fcd5cc981fa3e841f8c65aa0920530a9
Daria Prokofieva <dashaprokofieva95@gmail.com>
2019-09-02 10:10:52 +0200

    feat: add search

--- src/Search.js
+++ src/Search.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+import Book from './Book'
 
 class Search extends React.Component {
   state = {
     query: '',
+    searchedBooks: []
   }
   updateQuery = (query) => {
     this.setState(() => ({
       query: query
     }))
+    if  (query){
+       BooksAPI.search(query).then((searchedBooks) => {
+         if (searchedBooks.error){
+           this.setState ({searchedBooks:[]});
+         }else{
+           this.setState(() => ({searchedBooks}));
+         }
+       })
+     } else {
+       this.setState ({searchedBooks:[]});
+     }
   }
   render() {
+    var that = this;
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -23,7 +38,15 @@ class Search extends React.Component {
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+           { this.state.searchedBooks.map((searchedBook) => (
+              <li key={searchedBook.id}>
+              <Book bookDetail={searchedBook}
+                    searchedBooks={that.state.searchedBooks}
+                    update={that.props.update}
+                    booksList={that.props.booksList}/>
+              </li>))}
+          </ol>
         </div>
       </div>
     )
